bb6f073e742238e8b7da03c2a4b010597c0be00a
jakubsadkowski <j.sadkowski@wp.pl>
2023-10-30 21:50:18 +0100

    zmiana

--- zadanialab3/zadanie5/main.c
+++ zadanialab3/zadanie5/main.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 
 int main() {
-    char input;
+    char input_char;
+    int input_code;
+
+    printf("Wprowadź znak ASCII (char) lub kod ASCII (int): ");
     
-    printf("Podaj znak ASCII lub kod ASCII: ");
-    scanf("%c", &input);
-    
-    if ((input >= 'A' && input <= 'Z') || (input >= 'a' && input <= 'z')) 
-        printf("To jest litera alfabetu.\n");
-    else 
-        printf("To nie jest litera alfabetu.\n");
-    
+    // Odczytaj wartość od użytkownika
+    if (scanf("%c", &input_char) == 1) {
+        // Sprawdź, czy to litera alfabetu
+        if ((input_char >= 'A' && input_char <= 'Z') || (input_char >= 'a' && input_char <= 'z')) {
+            printf("To jest litera alfabetu: %c\n", input_char);
+        } else {
+            printf("To nie jest litera alfabetu: %c\n", input_char);
+        }
+    } else if (scanf("%d", &input_code) == 1) {
+        // Sprawdź, czy to kod ASCII litery alfabetu
+        if ((input_code >= 65 && input_code <= 90) || (input_code >= 97 && input_code <= 122)) {
+            printf("To jest kod ASCII flitery alfabet90u: %c\n", (char)input_code);
+        } else {
+            printf("To nie jest kod ASCII litery alfabetu: %d\n", input_code);
+        }
+    } else {
+        printf("Błędne dane wejściowe.\n");
+    }
 
     return 0;
 }
