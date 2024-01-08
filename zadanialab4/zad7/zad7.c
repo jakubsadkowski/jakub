cbe2418ef226e3dae65c945cee9ca731c1048e92
jakubsadkowski <j.sadkowski@wp.pl>
2024-01-08 21:40:18 +0100

    zad7

--- zadanialab4/zad7/zad7.c
+++ zadanialab4/zad7/zad7.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+bool pierwsza(int n){
+    for (int i = 2; i*i <= n; i++)
+    {
+        if (n%i==0)
+        {
+            return false;
+        }  
+    }
+    return true;
+}
 
 int main(){
-    printf("Hello World");
+    int n;
+    printf("Podaj liczbę n: ");
+    scanf("%d", &n);
+    printf("\n");
+
+    if (n<2)
+    {
+        printf("Nie jest to liczba pierwsza");
+    }
+    else
+    {
+        if (pierwsza(n))
+        {
+            printf("Jest to liczba pierwsza");
+        }
+        else
+        {
+            printf("Nie jest to liczba pierwsza");
+        }
+        
+        
+    }
+    
+        
+    
     return 0;
 }
