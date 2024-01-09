4dc7968e7c3fe55f363005c5b5f709bd9fa92b9b
jakubsadkowski <j.sadkowski@wp.pl>
2024-01-09 21:03:42 +0100

    a

--- zadanialab7/zad1/zad1.c
+++ zadanialab7/zad1/zad1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int g=9;
+int nw(int a, int b){
+    printf("a: %p b: %p\n", &a, &b);
+}
+
 int main(){
     printf("Hello World!\n");
     int p=g;
     printf("g: %p p: %p\n", &g, &p);
     nw(g,p);
     return 0;
-}
-int nw(int a, int b){
-    printf("a: %p b: %p\n", &a, &b);
 }
